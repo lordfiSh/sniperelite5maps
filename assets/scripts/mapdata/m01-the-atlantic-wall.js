197a988f1de3e4161d48c1ae5c45ec0fe8f0f308
AtomCrafty <atomcrafty@frucost.net>
2022-05-26 20:22:38 +0200

    set map parameters for m01

--- assets/scripts/mapdata/m01-the-atlantic-wall.js
+++ assets/scripts/mapdata/m01-the-atlantic-wall.js
@@ -1,10 +1,11 @@
 app.mapData = {
 	name: 'm01-the-atlantic-wall',
 	tilePath: 'maps/m_coast/{z}/{x}/{y}.png',
-	minZoom: 2,
-	maxZoom: 6,
-	defaultZoom: 3,
-	dimensions: [4096, 4096],
+	minZoom: 1,
+	maxZoom: 4,
+	neutralZoom: 4, // the zoom level where the combined tile size equals the dimensions below
+	defaultZoom: 2,
+	dimensions: [3840, 3840],
 	//focus: [100, 100]
 };
 
