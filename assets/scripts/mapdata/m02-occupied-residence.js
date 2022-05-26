3f5980bf6553b4660fc608ca277a1ae3bac62e32
lordfiSh <wst.lordfish@gmail.com>
2022-05-26 12:55:50 +0200

    m02 makers (1/2)

--- assets/scripts/mapdata/m02-occupied-residence.js
+++ assets/scripts/mapdata/m02-occupied-residence.js
@@ -10,23 +10,25 @@ app.mapData = {
 };
 
 app.mapData.markers = [
-	makeMarker('letter', 1),
-	makeMarker('letter', 2),
-	makeMarker('letter', 3),
-	makeMarker('letter', 4),
-	makeMarker('document', 1),
-	makeMarker('document', 2),
-	makeMarker('document', 3),
-	makeMarker('document', 4),
-	makeMarker('document', 5),
+	makeMarker('letter', 1, 2033,1666),
+	makeMarker('letter', 2, 2188,1666),
+	makeMarker('letter', 3, 1981,1760),
+	makeMarker('letter', 4, 2214,2078),
+	makeMarker('document', 1, 2030,1684), // also 2119,1272 & 1516,1636
+	makeMarker('document', 2, 2013,1598),
+	makeMarker('document', 3, 2041,1610),
+	makeMarker('document', 4, 1710,2326),
+	makeMarker('document', 5, 1672,2383),
 	makeMarker('document', 6),
 	makeMarker('personal-item', 1),
 	makeMarker('personal-item', 2),
 	makeMarker('personal-item', 3),
-	makeMarker('dead-eye-target', 1),
-	makeMarker('dead-eye-target', 2),
-	makeMarker('dead-eye-target', 3),
+	makeMarker('dead-eye-target', 1, 2202,2155),
+	makeMarker('dead-eye-target', 2, 1702,1390),
+	makeMarker('dead-eye-target', 3, 1898,823),
+	makeMarker('workbench', 1, 1702,2341),
 	makeMarker('workbench', 0),
 	makeMarker('workbench', 0),
-	makeMarker('workbench', 0),
+
+
 ];
